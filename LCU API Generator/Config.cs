6996d761bf7ead5fa4e633e9c6be66d88f6b0899
Felipe Martinez <pipernene@gmail.com>
2018-12-10 18:11:44 +0100

    Added config class

---
+++ LCU API Generator/Config.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCU_API_Generator
+{
+    public class Config
+    {
+        [JsonProperty("modelNamespace")]
+        public string ModelNamespace { get; set; }
+
+        [JsonProperty("interfaceNamespace")]
+        public string InterfaceNamespace { get; set; }
+
+        [JsonProperty("outFolder")]
+        public string OutFolder { get; set; }
+
+        [JsonProperty("classVisibility")]
+        public string ClassVisibility { get; set; }
+    }
+}
